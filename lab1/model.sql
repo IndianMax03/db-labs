5354b3dcd1bedc6714f1418431a9c740356a6dd5
IndianMax03 <vermamanu@mail.ru>
2023-10-17 20:47:12 +0300

    Fix: naming and notes

--- lab1/model.sql
+++ lab1/model.sql
@@ -1,14 +1,14 @@
 create table if not exists astronauts (
-    id serial primary key,
+    id serial primary key, --  fix in model
     name varchar(32) not null,
     sex varchar(1),
     height real,
     weight real,
-    stress_resistance smallint,
+    stress_resistance smallint
 );
 
 create table if not exists roles (
-    id serial primary key,
+    id serial primary key, --  fix in model
     astronaut_id integer references astronauts,
     name varchar(32),
     assignment_date timestamp
@@ -20,27 +20,27 @@ create table if not exists ais (
 );
 
 create table if not exists rockets (
-    id serial primary key,
+    id serial primary key, --  fix in model
     name varchar(32) not null,
     has_manual_control boolean,
     ai_name varchar(32) references ais
 );
 
 create table if not exists planets (
-    id serial primary key,
+    id serial primary key, --  fix in model
     name varchar(64),
     has_atmosphere boolean,
     temperature real
 );
 
 create table if not exists space_expeditions (
-    id serial primary key,
+    id serial primary key, --  fix in model
     name varchar(32) not null,
     rocket_id integer references rockets,
     origin_planet_id integer references planets,
     destination_planet_id integer references planets,
-    start timestamp,
-    end timestamp,
+    opening timestamp, --  fix in model
+    closure timestamp, --  fix in model
     is_success boolean
 );
 
