4b6cdd51e4cea8e01f5ef265aa31ab104052b62b
IndianMax03 <vermamanu@mail.ru>
2023-10-19 00:13:34 +0300

    Update: sql model

--- lab1/model.sql
+++ lab1/model.sql
@@ -1,51 +1,60 @@
 create table if not exists astronauts (
-    id serial primary key, --  fix in model
+    id serial primary key,
     name varchar(32) not null,
-    sex varchar(1),
+    surname varchar(32) not null,
+    sex varchar(6),
     height real,
     weight real,
-    stress_resistance smallint
+    stress_resistance smallint,
+    constraint valid_sex check (sex = 'male' or sex = 'female'),
+    constraint valid_height check (height > 0),
+    constraint valid_weight check (weight > 0)
 );
 
 create table if not exists roles (
-    id serial primary key, --  fix in model
-    astronaut_id integer references astronauts,
-    name varchar(32),
+    id serial primary key,
+    astronaut_id integer references astronauts (id),
+    name varchar(32) not null,
     assignment_date timestamp
 );
 
 create table if not exists ais (
-    name varchar(32) primary key,
-    version real
+    id serial primary key,
+    name varchar(32) not null,
+    version real,
+    constraint valid_version check (version >= 0)
 );
 
 create table if not exists rockets (
-    id serial primary key, --  fix in model
+    id serial primary key,
     name varchar(32) not null,
     has_manual_control boolean,
-    ai_name varchar(32) references ais
+    ai_id integer references ais (id)
 );
 
 create table if not exists planets (
-    id serial primary key, --  fix in model
-    name varchar(64),
+    id serial primary key,
+    name varchar(64) not null,
     has_atmosphere boolean,
     temperature real
 );
 
 create table if not exists space_expeditions (
-    id serial primary key, --  fix in model
+    id serial primary key,
     name varchar(32) not null,
-    rocket_id integer references rockets,
-    origin_planet_id integer references planets,
-    destination_planet_id integer references planets,
-    opening timestamp, --  fix in model
-    closure timestamp, --  fix in model
-    is_success boolean
+    rocket_id integer references rockets (id),
+    origin_planet_id integer references planets (id),
+    destination_planet_id integer references planets (id),
+    opening timestamp,
+    closure timestamp,
+    is_success boolean,
+    constraint valid_route check (origin_planet_id != destination_planet_id),
+    constraint valid_time check (opening <= closure)
 );
 
 create table if not exists expedition_crew (
-    astronaut_id integer references astronauts,
-    space_expedition_id integer references space_expeditions,
-    experience integer
+    astronaut_id integer references astronauts (id),
+    space_expedition_id integer references space_expeditions (id),
+    experience integer,
+    constraint valid_experience check (experience >= 0)
 );
